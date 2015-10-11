5e59c8e77783b78ab0e509aede3f8ae91b430769
alinddicu <alinddicu@yahoo.com>
2015-10-11 17:04:33 +0200

    multilanguage viewmodel

--- mariage/mariage/WebPage/scripts/MainViewModel.js
+++ mariage/mariage/WebPage/scripts/MainViewModel.js
@@ -3,7 +3,34 @@ var alindicu = window.alindicu = window.alindicu || {};
 alindicu.MainViewModel = function () {
     var self = this;
 
-    self.mariage = ko.observable(new alindicu.MariageViewModel('le 2 Juillet 2016'));
+    self.heureMairie = 'dans la matinée (*)';
+    self.heureEglise = 'dans l\'après-midi (*)';
+    self.heureRestaurant = 'le soir (*)';
+
+    self.mariage = ko.observable(new alindicu.MariageViewModel(
+        'le 2 Juillet 2016',
+        'à Lyon',
+        'A la mairie',
+        'La cérémonie se tiendra ',
+        ' à la',
+        'Mairie de Villeurbanne',
+        '3, Place du Dr Lazare Goujon, 69100 Villeurbanne',
+        'Parkings payants',
+        '17 rue Michel Servet (air libre)',
+        '38 bis rue Michel Servet (couvert)',
+        'A l\'église',
+        'La cérémonie se tiendra ',
+        ' à',
+        'l\'Eglise Saint Côme Saint Damien',
+        '53, chemin de Fond Rose, Caluire et Cuire, 69300',
+        'Parking gratuit : dans la cour de l\'église',
+        'Au restaurant',
+        'Nous allons faire la fête ',
+        ' au',
+        'Restaurant  "Chez nous"',
+        'Route  Départementale  D61  (01)  Ain, Sainte Croix, 01120',
+        'Parking gratuit : devant le restaurant',
+        '(*)Les heures exactes seront mises à jour prochainement'));
 
     new alindicu.GoogleMap('map-mairie', 45.766450, 4.879592);
     new alindicu.GoogleMap('map-eglise', 45.791826, 4.824995);
--- mariage/mariage/WebPage/scripts/MariageViewModel.js
+++ mariage/mariage/WebPage/scripts/MariageViewModel.js
@@ -1,8 +1,53 @@
 var alindicu = window.alindicu = window.alindicu || {};
 
-alindicu.MariageViewModel = function (mariageDate) {
+alindicu.MariageViewModel = function (
+    date,
+    lieu,
+    aLaMairie,
+    ceremonieALaMairie,
+    ceremonieMairieALaMairie,
+    mairieLabel,
+    adresseMairie,
+    mairieParkingLabel,
+    mairieParking1,
+    mairieParking2,
+    aLEglise,
+    ceremonieALEglise,
+    ceremonieALEgliseALEglise,
+    egliseLabel,
+    adresseEglise,
+    parkingEglise,
+    auRestaurant,
+    feteRestaurant,
+    feteRestaurantAu,
+    restaurantLabel,
+    adresseRestaurant,
+    parkingRestaurant,
+    disclaimerMajHeures) {
 
     var self = this;
 
-    self.date = mariageDate;
+    self.date = date;
+    self.lieu = lieu;
+    self.aLaMairie = aLaMairie;
+    self.ceremonieALaMairie = ceremonieALaMairie;
+    self.ceremonieMairieALaMairie = ceremonieMairieALaMairie;
+    self.mairieLabel = mairieLabel;
+    self.adresseMairie = adresseMairie;
+    self.mairieParkingLabel = mairieParkingLabel;
+    self.mairieParking1 = mairieParking1;
+    self.mairieParking2 = mairieParking2;
+    self.aLEglise = aLEglise;
+    self.ceremonieALEglise = ceremonieALEglise;
+    self.ceremonieALEgliseALEglise = ceremonieALEgliseALEglise;
+    self.egliseLabel = egliseLabel;
+    self.adresseEglise = adresseEglise;
+    self.parkingEglise = parkingEglise;
+    self.auRestaurant = auRestaurant;
+    self.feteRestaurant = feteRestaurant;
+    self.feteRestaurantAu = feteRestaurantAu;
+    self.restaurantLabel = restaurantLabel;
+    self.adresseRestaurant = adresseRestaurant;
+    self.parkingRestaurant = parkingRestaurant;
+    self.disclaimerMajHeures = disclaimerMajHeures;
 };
