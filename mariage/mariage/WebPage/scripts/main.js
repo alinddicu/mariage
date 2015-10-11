b9d3e50c6786e3ac8de4dac70456cd280dc45f94
alinddicu <alinddicu@yahoo.com>
2015-10-11 11:11:22 +0200

    fixed bug

--- mariage/mariage/WebPage/scripts/main.js
+++ mariage/mariage/WebPage/scripts/main.js
@@ -4,6 +4,8 @@ alindicu.GoogleMap = function(divId, latitude, longitude) {
     new google.maps.Map(document.getElementById(divId), {
         center: new google.maps.LatLng(latitude, longitude),
         zoom: 18,
+        scrollwheel: false,
+        mapTypeControl: false,
         mapTypeId: google.maps.MapTypeId.HYBRID
     });
 };
